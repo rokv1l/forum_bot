779d10a854e7c2819aca7157446b3b46ca3292dc
Leo <guzeev.leonid.dc@gmail.com>
2022-05-29 00:33:00 +0300

    f

--- bot/tmp.py
+++ bot/tmp.py
@@ -4,4 +4,4 @@ from src.database import users_col
 
 users = users_col.find()
 for user in users:
-    users_col.update_one({'_id': user["_id"]}, {'$set': {'code': user['code'].replace(' ', '')}})
+    users_col.update_one({'_id': user["_id"]}, {'$set': {'code': user['code'].replace(' ', '') if user['code'] else ''}})
