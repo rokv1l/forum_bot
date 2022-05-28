9c66625cd8b342f062a99520d50421ee3dd93be3
Leo <guzeev.leonid.dc@gmail.com>
2022-05-28 17:24:33 +0300

    f

--- bot/bot_modules/user_modules/main_user_menu.py
+++ bot/bot_modules/user_modules/main_user_menu.py
@@ -66,10 +66,10 @@ def auth_code(update, context):
                 'code': '',
                 'platform': ''
             })
-            update.effective_chat.send_message('Вы авторизованны и наделены правами администратора, для входа в меню администратора воспользуйтесь /admin')
+            update.effective_chat.send_message('Вы авторизованны и наделены правами администратора, для входа в меню администратора воспользуйтесь /admin\nДля просмотра функционала пользователя воспользуйтесь /start')
         elif not not user['admin']:
             users_col.update_one({'tg_id': update.effective_chat.id}, {'$set': {'admin': True}})
-            update.effective_chat.send_message('Вы наделены правами администратора, для входа в меню администратора воспользуйтесь /admin')
+            update.effective_chat.send_message('Вы наделены правами администратора, для входа в меню администратора воспользуйтесь /admin\nДля просмотра функционала пользователя воспользуйтесь /start')
 
     else:
         update.effective_chat.send_message('Вы ввели недействительный код, проверьте правильность написания и попробуйте еще раз')
