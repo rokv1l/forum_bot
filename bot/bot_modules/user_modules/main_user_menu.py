79996eb6dfbe05f45b7b2da80067082c1d1525f3
Leo <guzeev.leonid.dc@gmail.com>
2022-05-29 00:19:14 +0300

    f

--- bot/bot_modules/user_modules/main_user_menu.py
+++ bot/bot_modules/user_modules/main_user_menu.py
@@ -31,7 +31,7 @@ def start(update, context):
 
 
 def auth_code(update, context):
-    code = re.compile(update.message.text.replace(' ', '( )?'), re.IGNORECASE)
+    code = re.compile(update.message.text.replace(' ', '\s?'))
     user = users_col.find_one({'code': code})
     if user:
         context.user_data['autorized'] = True
